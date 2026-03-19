465948359671cf521a0e0d5ed3f3b545275926e8
Ronald Reignz <ronaldreignz02@gmail.com>
2026-03-19 22:40:17 +0300

    fixed hostname with full domain

--- db_connect.php
+++ db_connect.php
@@ -3,13 +3,13 @@ error_reporting(E_ALL);
 ini_set('display_startup_errors', 1);
 ini_set('display_errors', 1);
 
-$host = "dpg-d6u1c1i4d50c73ck19eg-a";
+$host = "dpg-d6u1c1i4d50c73ck19eg-a.frankfurt-postgres.render.com";
 $port = "5432";
 $dbname = "groups_db_awm4";
 $user = "groups_db_awm4_user";
 $password = "zgCCRjPp6mK7E02ZuuP6rLPnJfYhacZO";
 
- $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+ $conn = pg_connect("host=$host port=5432 dbname=$dbname user=$user password=$password");
 
  if (!$conn) {
      die("Connection failed: " . pg_last_error());
